b0c0b1712b7898125500de0441cf44099b3640a2
Novee <77930262+Veenoway@users.noreply.github.com>
2025-03-12 20:24:06 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,18 @@
-import { Home } from "@/features/home";
-
 export default function HomePage() {
-  return <Home />;
+  return (
+    <main
+      className="w-screen flex items-center justify-center flex-col min-h-screen pb-[100px] font-montserrat"
+      style={{
+        background: "black",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        fontFamily: "Boogaloo",
+      }}
+    >
+      <h1 className="text-3xl lg:text-6xl font-bold">Will be back soon</h1>
+      <p className="mt-4 text-lg lg:text-2xl">
+        We are currently performing maintenance. Please check back later.
+      </p>
+    </main>
+  );
 }
